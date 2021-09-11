cd6ff8d490b834e91456d3711f9157e0b8dd8ef7
DanilWaliev <dnlvlv39@gmail.com>
2021-09-11 14:54:05 +0500

    firstCommit

---
+++ script.js
@@ -0,0 +1,100 @@
+﻿// let numb1 = +prompt('1 число');
+// let numb2 = +prompt('2 число');
+
+// alert(numb1 + numb2);
+
+
+
+
+
+// function readNumber() {
+//         let numb = prompt('Введите число');
+//         return numb;
+//     } while (numb!==null || numb !== Number);
+// }
+
+// alert ('Число:' + readNumber() );
+
+
+
+// let num1 = +prompt ('Введите число')
+// alert( Math.floor(num1) )
+
+
+
+// alert( isNaN(234) )
+// alert( isFinite(Nan) )
+// alert( isFinite(Infinity) )
+// alert( isFinite(234) )
+
+
+
+// alert( parseInt('02384234jkvnj') )
+
+
+
+
+
+// let str = prompt('Type a string')
+// alert( ucFirst(str) )
+
+// function ucFirst(str) {
+//     if (!str) return str;
+
+//     return str[0].toUpperCase() + str.slice(1)
+// }
+
+
+
+// let str = 'hanahanaxahaxa'
+// let target = 'ha'
+// let pos = 0
+
+// while (true) {
+//     foundPos = str.indexOf(target, pos);
+//     if (foundPos == -1) break;
+
+//     console.log(`Найдено тут :; ${foundPos}`)
+//     pos = foundPos + 1;
+// }
+
+
+
+// function checkSpam(str) {
+//     let upperStr = str.toUpperCase();
+
+//     if (upperStr.indexOf('VIAGRA') !== -1 ||
+//         upperStr.indexOf('XXXXX') !== -1
+//     ) {
+//         return true;
+//     } else {
+//         return false;
+//     };
+// };
+
+// console.log( checkSpam('buy ViAgRA now') );
+// console.log( checkSpam('free xxxxx') );
+// console.log( checkSpam('innocent rabbit') );
+
+
+
+// function truncate(str, maxlength) {
+//     if ( str.length >= maxlength ) {
+//         return (str.slice(0, maxlength - 1) + '...');
+//     } else {
+//         return str
+//     };
+// };
+
+// console.log( truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) );
+// console.log( truncate("Всем привет!", 20) );
+
+
+
+
+
+d
+
+
+
+
